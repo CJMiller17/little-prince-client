923ea82655d1f1b254a34230c8c31cd4c581407a
Cody <c.jmiller17@yahoo.com>
2024-06-20 11:07:03 -0400

    Moving to dev

--- src/ConceitedMan/Home.jsx
+++ src/ConceitedMan/Home.jsx
@@ -1,18 +1,25 @@
+import { Button, ButtonGroup, Stack } from "@chakra-ui/react";
 import "./Conceited.css";
 
 const Home = ({ onGame }) => {
   return (
-    
     <div className="home">
-        <div className="title">The Conceited Man</div>
-        <div className="author">
-            Asteroid 326
-        </div>
-        <button onClick={() => onGame("playGame")} className="btnPlay">
-            Play Game
-        </button>
-    </div>
+      <div className="title">The Conceited Man</div>
+      <div className="author">Asteroid 326</div>
+      <Stack direction="column" spacing="1"> 
+        <Button onClick={() => onGame("playGame")} className="btnPlay">
+          Play Game
+        </Button>
 
+        <Button
+          size="xs"
+          onClick={() => onGame("playGame")}
+          className="btnPlay"
+        >
+          Rules
+        </Button>
+      </Stack>
+    </div>
   );
 };
 
