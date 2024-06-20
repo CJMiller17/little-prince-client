c2b06bb7e6bcde653fc265340ee8471e7d9973c8
Cody <c.jmiller17@yahoo.com>
2024-06-20 10:07:19 -0400

    Pictures are working

--- src/GamePage.jsx
+++ src/GamePage.jsx
@@ -10,54 +10,64 @@ import { Link, useNavigate } from "react-router-dom";
 import { CgProfile } from "react-icons/cg";
 import { getCurrentUserProfile } from "./apis";
 import { AuthContext } from "./ContextProvider";
+import Rose from '../src/public/assets/princes_planet.jpg'
+import King from "../src/public/assets/kings_planet.jpg";
+import Conceited from "../src/public/assets/conceited_planet.jpg";
+import Drunkard from "../src/public/assets/drunkard_planet.jpg";
+import Business from "../src/public/assets/businessman_planet.jpg";
+import Lamplighter from "../src/public/assets/lamplighter_planet.jpg";
+import Geographer from "../src/public/assets/geographer_planet.jpg";
+import Earth from "../src/public/assets/earth.jpg";
+
+
 
 const planets = [
   {
     id: 1,
     name: "Rose",
-    image: "src/public/assets/princes_planet.jpg", // Why does the name appear behind the image?
+    image: Rose, // Why does the name appear behind the image?
     gameRoute: "/rose",
   },
   {
     id: 2,
     name: "King",
-    image: "src/public/assets/kings_planet.jpg", // Why does the name appear behind the image?
+    image: King, // Why does the name appear behind the image?
     gameRoute: "/king",
   },
   {
     id: 3,
     name: "Conceited Man",
-    image: "src/public/assets/conceited_planet.jpg",
+    image: Conceited,
     gameRoute: "/conceited",
   },
   {
     id: 4,
     name: "Drunkard",
-    image: "src/public/assets/drunkard_planet.jpg",
+    image: Drunkard,
     gameRoute: "/drunkard",
   },
   {
     id: 5,
     name: "Business Man",
-    image: "src/public/assets/businessman_planet.jpg",
+    image: Business,
     gameRoute: "/business",
   },
   {
     id: 6,
     name: "Lamplighter",
-    image: "src/public/assets/lamplighter_planet.jpg",
+    image: Lamplighter,
     gameRoute: "/lamplighter",
   },
   {
     id: 7,
     name: "Geographer",
-    image: "src/public/assets/geographer_planet.jpg",
+    image: Geographer,
     gameRoute: "/geographer",
   },
   {
     id: 8,
     name: "Earth",
-    image: "src/public/assets/earth.jpg",
+    image: Earth,
     gameRoute: "/earth",
   },
 ];
@@ -95,6 +105,12 @@ const GamePage = () => {
         height="100vh"
         // backgroundColor="black"
       >
+        <IconButton
+          aria-label="upload profile image"
+          icon={<CgProfile />}
+          isRound
+          onClick={handleSubmit}
+        />
         {planets.map((planet, index) => (
           <Box
             key={planet.id}
@@ -102,7 +118,6 @@ const GamePage = () => {
             top={`${index * 20 + 10}%`}
             left={`${index * 10 + 10}%`}
             transform="translate(-50%, -50%)"
-            
           >
             <Button
               onClick={() => handlePlanetClick(planet.gameRoute)}
@@ -111,7 +126,12 @@ const GamePage = () => {
               bg="none"
               color="black"
             >
-              <Image src={planet.image} alt={planet.name} boxSize="200px" borderRadius="10%" />
+              <Image
+                src={planet.image}
+                alt={planet.name}
+                boxSize="200px"
+                borderRadius="10%"
+              />
             </Button>
             {index < planets.length - 1 && (
               <Box
@@ -126,13 +146,6 @@ const GamePage = () => {
           </Box>
         ))}
       </Box>
-
-      <IconButton
-        aria-label="upload profile image"
-        icon={<CgProfile />}
-        isRound
-        onClick={handleSubmit}
-      />
     </>
   );
 };
